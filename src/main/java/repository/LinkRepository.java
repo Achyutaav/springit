eb8e60289b80d947b3a3438034cafc296b303368
Achyutaav <achyut.nci@gmail.com>
2021-02-18 23:41:22 +0000

    Initial Repositories for Link and Comment

---
+++ src/main/java/repository/LinkRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import com.achyut.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+}
